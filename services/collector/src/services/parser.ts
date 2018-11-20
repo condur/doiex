bd3aa11bac81fef76e87569c3c6d32d7d16b60df
Dima Condur <kdumitru@gmail.com>
2018-11-19 19:55:50 -0700

    removed from collector the webpack dependencie

--- services/collector/src/services/parser.ts
+++ services/collector/src/services/parser.ts
@@ -1,24 +1,10 @@
-import * as request from 'request-promise'
+import request from 'request-promise'
 
 /**
  * Send data to parser service
  * @param {Object} // data collected
  */
-export let send = (data) => {
-  if (process.env.NODE_ENV === 'production') {
-    return sendProductionData(data)
-  } else {
-    return sendDevelopmentData(data)
-  }
-}
-
-/**
- * Send data to parser service when the process.env.NODE_ENV == 'production'
- *
- * @private
- * @param {Object} data
- */
-function sendProductionData (data) {
+export let send  = (data) : request.RequestPromise => {
   // Http request options
   let options = {
     method: 'PUT',
@@ -27,14 +13,4 @@ function sendProductionData (data) {
     json: true // Automatically parses the JSON string in the response
   }
   return request(options)
-}
-
-/**
- * Send data to parser service when the process.env.NODE_ENV != 'production'
- *
- * @private
- * @param {Object} data
- */
-function sendDevelopmentData (data) {
-  console.log('In NON-production mode the Parser Service is not called')
 }
